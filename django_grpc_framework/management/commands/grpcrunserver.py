9a970c4013e0e2f64015926e3695f9b1322000d7
Jaume G <jaume.grane@fundcraft.lu>
2024-12-23 17:05:11 +0100

    imp asyncio server

--- django_grpc_framework/management/commands/grpcrunserver.py
+++ django_grpc_framework/management/commands/grpcrunserver.py
@@ -1,20 +1,18 @@
 # -*- coding: utf-8 -*-
-from concurrent import futures
+import asyncio
 from datetime import datetime
 import sys
-import errno
 import os
+import errno
 
 import grpc
 from django.utils import autoreload
 from django.conf import settings
 from django.core.management.base import BaseCommand
-
 from django_grpc_framework.settings import grpc_settings
 
-
 class Command(BaseCommand):
-    help = 'Starts a gRPC server.'
+    help = 'Starts an asynchronous gRPC server.'
 
     # Validation is called explicitly each time the server is reloaded.
     requires_system_checks = []
@@ -31,7 +29,7 @@ class Command(BaseCommand):
         parser.add_argument(
             '--dev', action='store_true', dest='development_mode',
             help=(
-                'Run the server in development mode.  This tells Django to use '
+                'Run the server in development mode. This tells Django to use '
                 'the auto-reloader and run checks.'
             )
         )
@@ -40,9 +38,9 @@ class Command(BaseCommand):
         self.address = options['address']
         self.development_mode = options['development_mode']
         self.max_workers = options['max_workers']
-        self.run(**options)
+        asyncio.run(self.run(**options))
 
-    def run(self, **options):
+    async def run(self, **options):
         """Run the server, using the autoreloader if needed."""
         if self.development_mode:
             if hasattr(autoreload, "run_with_reloader"):
@@ -50,30 +48,31 @@ class Command(BaseCommand):
             else:
                 autoreload.main(self.inner_run, None, options)
         else:
-            self.stdout.write((
-                "Starting gRPC server at %(address)s\n"
-            ) % {
-                "address": self.address,
-            })
-            self._serve()
+            self.stdout.write(f"Starting gRPC server at {self.address}\n")
+            await self._serve()
 
-    def _serve(self):
-        server = grpc.server(futures.ThreadPoolExecutor(max_workers=self.max_workers),
-                             interceptors=grpc_settings.SERVER_INTERCEPTORS)
+    async def _serve(self):
+        server = grpc.aio.server(
+            options=grpc_settings.SERVER_OPTIONS,
+            interceptors=grpc_settings.SERVER_INTERCEPTORS
+        )
         grpc_settings.ROOT_HANDLERS_HOOK(server)
         server.add_insecure_port(self.address)
-        server.start()
-        server.wait_for_termination()
+
+        self.stdout.write("gRPC server started and listening.")
+        await server.start()
+
+        try:
+            await server.wait_for_termination()
+        except asyncio.CancelledError:
+            self.stdout.write("Shutting down gRPC server...")
+            await server.stop(grace=None)
 
     def inner_run(self, *args, **options):
-        # If an exception was silenced in ManagementUtility.execute in order
-        # to be raised in the child process, raise it now.
         autoreload.raise_last_exception()
 
         self.stdout.write("Performing system checks...\n\n")
         self.check(display_num_errors=True)
-        # Need to check migrations here, so can't use the
-        # requires_migrations_check attribute.
         self.check_migrations()
         now = datetime.now().strftime('%B %d, %Y - %X')
         self.stdout.write(now)
@@ -88,21 +87,17 @@ class Command(BaseCommand):
             "address": self.address,
             "quit_command": quit_command,
         })
+
         try:
-            self._serve()
+            asyncio.run(self._serve())
         except OSError as e:
-            # Use helpful error messages instead of ugly tracebacks.
             ERRORS = {
                 errno.EACCES: "You don't have permission to access that port.",
                 errno.EADDRINUSE: "That port is already in use.",
                 errno.EADDRNOTAVAIL: "That IP address can't be assigned to.",
             }
-            try:
-                error_text = ERRORS[e.errno]
-            except KeyError:
-                error_text = e
-            self.stderr.write("Error: %s" % error_text)
-            # Need to use an OS exit because sys.exit doesn't work in a thread
+            error_text = ERRORS.get(e.errno, str(e))
+            self.stderr.write(f"Error: {error_text}")
             os._exit(1)
         except KeyboardInterrupt:
             sys.exit(0)
